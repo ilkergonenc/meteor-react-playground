0a13e369fdd18826f47fcd2f34dfe1aec8b572e3
ig <ilkergonenc@gmail.com>
2022-07-08 20:19:33 +0300

    without middleware

--- imports/ui/routes/NotFound.jsx
+++ imports/ui/routes/NotFound.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useLayout } from "../providers/LayoutProvider";
 
 export default function NotFound() {
+  const layout = useLayout();
+
+  if (layout) {
+    layout.setLayout({
+      ...layout.settings,
+      layout: "cover-body",
+      page: {
+        ...layout.settings.page,
+        layout: "cover-page h-full flex justify-center items-center",
+        main: "",
+      },
+    });
+  }
   return (
     <div>
       <p className="mb-6">Not Found</p>
