22cac316e833a07c8d1be16decd69dbe6e0cd20f
ig <ilkergonenc@gmail.com>
2022-07-13 13:00:50 +0300

    define form css classed

--- imports/ui/components/FormControl.jsx
+++ imports/ui/components/FormControl.jsx
@@ -6,12 +6,7 @@ export function FormControl(props) {
 
   return (
     <div className="relative">
-      <label
-        htmlFor={handle}
-        className="
-          absolute left-0 top-0 pl-3 pt-1.5 text-gray-600 text-xs
-        "
-      >
+      <label htmlFor={handle} className="form-label">
         {label}
       </label>
       <input
@@ -20,24 +15,15 @@ export function FormControl(props) {
         id={handle}
         name={handle}
         placeholder={holder}
-        className="
-          w-full px-3 pt-6 pb-2 border rounded-md
-          border-gray-300 placeholder-transparent text-gray-500 
-          focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 
-        "
+        className="form-input"
       />
       {helper && (
-        <div
-          id={handle + "Help"}
-          className="form-text text-xs flex text-gray-600"
-        >
+        <div id={handle + "Help"} className="form-text-helper">
           {helper}
         </div>
       )}
       {errors[handle] && (
-        <span className="flex mt-2 text-sm text-red-400">
-          This field is required
-        </span>
+        <span className="form-text-error">This field is required</span>
       )}
     </div>
   );
