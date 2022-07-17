9d562114a16ef90145f1a82d16bd82093aa9c98d
ig <ilkergonenc@gmail.com>
2022-07-17 20:20:51 +0300

    imports tests

--- tests/main.js
+++ tests/main.js
@@ -1,20 +1,3 @@
-import assert from "assert";
+import "/imports/api/hosts/hosts.methods.test";
 
-describe("playground", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "playground");
-  });
-
-  if (Meteor.isClient) {
-    it("client is not server", function () {
-      assert.strictEqual(Meteor.isServer, false);
-    });
-  }
-
-  if (Meteor.isServer) {
-    it("server is not client", function () {
-      assert.strictEqual(Meteor.isClient, false);
-    });
-  }
-});
+// import "./startup";
