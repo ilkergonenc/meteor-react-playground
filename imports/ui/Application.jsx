e2549e7452a85a5d6a749b464876aaeb7c87a4e5
ig <ilkergonenc@gmail.com>
2022-07-17 19:31:12 +0300

    examples of method usage in client

--- imports/ui/Application.jsx
+++ imports/ui/Application.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Loadable } from "meteor/npdev:react-loadable";
 
-import { fetchHostIdFromAddress } from "../api/hosts/hosts.validMethods";
-
 const Loading = () => <p>Loading ...</p>;
 
 const Hello = Loadable({
@@ -15,12 +13,61 @@ const NotFound = Loadable({
   loading: Loading,
 });
 
-fetchHostIdFromAddress.call({ address: location.host }, (error, respond) => {
-  if (error) {
-    console.error(error);
-  }
-  console.log(respond);
-});
+// import {
+//   fetchHostIdFromAddress,
+//   insertHost,
+//   updateHost,
+//   removeHost,
+// } from "../api/hosts/hosts.validMethods";
+
+// if (!Meteor.userId()) {
+//   Meteor.loginWithPassword("noone", "01010101", (e) => {
+//     if (e) console.error(e);
+//   });
+// }
+// Meteor.logout();
+// console.log(Meteor.userId());
+
+// fetchHostIdFromAddress.call({ address: location.host }, (error, respond) => {
+//   if (error) {
+//     console.error(error);
+//   }
+//   console.log(respond);
+// });
+
+// insertHost.call(
+//   {
+//     address: "localhost.com",
+//     name: "Local Com",
+//     title: "Try new",
+//   },
+//   (error, respond) => {
+//     if (error) {
+//       console.error(error);
+//     }
+//     console.log(respond);
+//   }
+// );
+
+// updateHost.call(
+//   {
+//     hostId: "qBzwMzzzGJgtK5GT7",
+//     address: "localhost.com",
+//     name: "Local Command",
+//     title: "Try new",
+//   },
+//   (error, respond) => {
+//     if (error) {
+//       console.error(error);
+//     }
+//     console.log(respond);
+//   }
+// );
+
+// removeHost.call({ hostId: "qBzwMzzzGJgtK5GT7" }, (error, respond) => {
+//   if (error) console.error(error);
+//   console.log(respond);
+// });
 
 export default Application = () => (
   <div className="app">
