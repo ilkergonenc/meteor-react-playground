21fe7454aded567ee8f540f605bcec774d32476d
ig <ilkergonenc@gmail.com>
2022-07-22 16:13:29 +0300

    fix imports

--- imports/api/@/middlewares/index.js
+++ imports/api/@/middlewares/index.js
@@ -1,7 +1,7 @@
-import SchemaMiddleware from "./schemaMiddleware";
-import AuthMiddleware from "./authMiddleware";
-import UniqueKeysMiddleware from "./uniqueKeysMiddleware";
-import OwnerMiddleware from "./ownerMiddleware";
+import SchemaMiddleware from "./SchemaMiddleware";
+import AuthMiddleware from "./AuthMiddleware";
+import UniqueKeysMiddleware from "./UniqueKeysMiddleware";
+import OwnerMiddleware from "./OwnerMiddleware";
 
 export {
   SchemaMiddleware,
